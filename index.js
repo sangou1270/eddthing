3146a1280547e039bb84dcda50611745f8b9f160
Eddthing CEO <sangou1270@gmail.com>
2019-07-14 15:34:45 +0900

    README.md 추가,index.js 테스트

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4100;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+
+app.listen(PORT,handleListening);
